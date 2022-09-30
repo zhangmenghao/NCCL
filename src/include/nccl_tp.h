9a2913bb4c212e69e4eae1895bc733fef7dc5489
yuwenjing03 <yuwenjing03@kuaishou.com>
2022-09-30 11:54:30 +0800

    correct tp type

--- src/include/nccl_tp.h
+++ src/include/nccl_tp.h
@@ -12,17 +12,17 @@
 TRACEPOINT_EVENT(nccl, ncclIbv_post_send, TP_ARGS(uint32_t, qpnum, int, chunksize, char*, remoaddr),
                  TP_FIELDS(ctf_integer(uint32_t, qpnum, qpnum)          //qp number
                             ctf_integer(int, chunksize, chunksize)      //chunksize = 128kB / num of qp
-                            ctf_integer(char*, remoaddr, remoaddr)))    //remote addr data sent to
+                            ctf_string(remoaddr, remoaddr)))    //remote addr data sent to
 
 
 TRACEPOINT_EVENT(nccl, ncclIbv_post_recv, TP_ARGS(uint32_t, qpnum, char*, remoaddr),
                  TP_FIELDS(ctf_integer(uint32_t, qpnum, qpnum)         //qp number
-                            ctf_integer(char*, remoaddr, remoaddr)))   //remote addr data sent from
+                            ctf_string(remoaddr, remoaddr)))   //remote addr data sent from
 
 
 TRACEPOINT_EVENT(nccl, ncclIbv_poll_cq, TP_ARGS(uint32_t, qpnum, char*, remoaddr),
                  TP_FIELDS(ctf_integer(uint32_t, qpnum, qpnum) 
-                            ctf_integer(char*, remoaddr, remoaddr)))   
+                            ctf_string(remoaddr, remoaddr)))   
 
 
 TRACEPOINT_EVENT(nccl, ncclIbv_destroy_send_qp, TP_ARGS(uint32_t, qpnum),  
