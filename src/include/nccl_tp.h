5def023d7384c64fbe54882e94de114e5103729f
yuwenjing03 <yuwenjing03@kuaishou.com>
2022-09-30 19:09:58 +0800

    debug remoaddr

--- src/include/nccl_tp.h
+++ src/include/nccl_tp.h
@@ -7,13 +7,13 @@
 #if !defined(_NCCL_TP_H) || defined(TRACEPOINT_HEADER_MULTI_READ)
 #define _NCCL_TP_H
 
-#include "lttng/tracepoint.h"s
+#include "lttng/tracepoint.h"
 
 TRACEPOINT_EVENT(nccl, ncclIbv_post_send, TP_ARGS(uint32_t, qpnum, int, chunksize, char*, remoaddr),
                  TP_FIELDS(ctf_integer(uint32_t, qpnum, qpnum)          //qp number
                             ctf_integer(int, chunksize, chunksize)      //chunksize = 128kB / num of qp
                             ctf_string(remoaddr, remoaddr)))    //remote addr data sent to   
-
+                       
 
 TRACEPOINT_EVENT(nccl, ncclIbv_post_recv, TP_ARGS(uint32_t, qpnum, char*, remoaddr),
                  TP_FIELDS(ctf_integer(uint32_t, qpnum, qpnum)         //qp number
