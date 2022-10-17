d64abc59089f37a697116bf69bd7891331790a1f
yuwenjing03 <yuwenjing03@kuaishou.com>
2022-10-17 10:48:04 +0800

    rm transremoaddr

--- src/include/nccl_tp.h
+++ src/include/nccl_tp.h
@@ -12,17 +12,17 @@
 TRACEPOINT_EVENT(nccl, ncclIbv_post_send, TP_ARGS(uint32_t, qpnum, int, chunksize, char*, remoaddr),
                  TP_FIELDS(ctf_integer(uint32_t, qpnum, qpnum)          //qp number
                             ctf_integer(int, chunksize, chunksize)      //chunksize = 128kB / num of qp
-                            ctf_string(remoaddr, remoaddr)))    //remote addr data sent to   
+                            ctf_integer(uint32_t, remoaddr, remoaddr)))    //remote addr data sent to   
                        
 
 TRACEPOINT_EVENT(nccl, ncclIbv_post_recv, TP_ARGS(uint32_t, qpnum, char*, remoaddr),
                  TP_FIELDS(ctf_integer(uint32_t, qpnum, qpnum)         //qp number
-                            ctf_string(remoaddr, remoaddr)))   //remote addr data sent from
+                            ctf_integer(uint32_t, remoaddr, remoaddr)))   //remote addr data sent from
 
 
 TRACEPOINT_EVENT(nccl, ncclIbv_poll_cq, TP_ARGS(uint32_t, qpnum, char*, remoaddr),
                  TP_FIELDS(ctf_integer(uint32_t, qpnum, qpnum) 
-                            ctf_string(remoaddr, remoaddr)))   
+                            ctf_integer(uint32_t, remoaddr, remoaddr)))   
 
 
 TRACEPOINT_EVENT(nccl, ncclIbv_destroy_send_qp, TP_ARGS(uint32_t, qpnum),  
